f372148cb24ff30212e910f8e7a7600904383bba
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2023-10-29 14:05:43 -0300

    first commit

---
+++ src/test/java/br/com/arthouseserv/arthouseserv/ArthouseServApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthouseserv.arthouseserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthouseServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
